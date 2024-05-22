9b813b7a89a8334703eb81c6cf19b96c770a77ee
Spencer Faught <sgfaught1@gmail.com>
2024-05-22 09:26:56 -0700

    Add NavBar

--- portfolio_Spencer-Faught/src/App.jsx
+++ portfolio_Spencer-Faught/src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+//import './App.css'
+import NavBar from './NavBar'
 
 
 function App() {
@@ -6,8 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>Test again!</h1>
-      <h1>Test again!</h1>
+     <NavBar />
     </>
   )
 }
