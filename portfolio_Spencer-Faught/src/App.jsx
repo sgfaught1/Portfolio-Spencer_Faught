709897accf4adb4011fb9e238eb76ae3bc5b64b8
Spencer Faught <sgfaught1@gmail.com>
2024-05-23 13:56:26 -0700

    Create experience component - JSON introduced

--- portfolio_Spencer-Faught/src/App.jsx
+++ portfolio_Spencer-Faught/src/App.jsx
@@ -2,6 +2,8 @@ import styles from './App.module.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About  from './components/About/About'
+import Experience from './components/Experience/Experience'
+
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   )
 }
