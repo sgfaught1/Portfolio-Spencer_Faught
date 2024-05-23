f5e55d6bfcd6bdafd5d99d63cfe48d385b15739e
Spencer Faught <sgfaught1@gmail.com>
2024-05-23 12:30:43 -0700

    About summary added

--- portfolio_Spencer-Faught/src/App.jsx
+++ portfolio_Spencer-Faught/src/App.jsx
@@ -1,7 +1,7 @@
 import styles from './App.module.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
-import { About } from './components/About/About'
+import About  from './components/About/About'
 
 
 function App() {
--- portfolio_Spencer-Faught/src/components/About/About.jsx
+++ portfolio_Spencer-Faught/src/components/About/About.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
+import styles from "./About.module.css"
 import { getImageUrl } from '../../utils'
 
+
 export default function About() {
-  return (
-    <section>
-        <h2>About</h2>
-        <div>
-            <img src={getImageUrl("about/aboutImage.png")} alt="Me with a laptop" />
-        </div>
-    </section>
-  )
+    return (
+        <section className = {styles.container} id = 'about'>
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
+                <img className={styles.aboutImage} src={getImageUrl("about/aboutImage.png")} alt="Me with a laptop" />
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Frontend Developer</h3>
+                            <p>I'm a frontdeveloper with experience in building responsive and optimized sites.</p>
+
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Backend Developer</h3>
+                            <p>I have experience developing experience in building fast and optimized API's.</p>
+
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/uiIcon.png")} alt="UI Icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>UI Designer</h3>
+                            <p>I have built multiple landing pages and have experience creating design systems as well.</p>
+
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
 }
