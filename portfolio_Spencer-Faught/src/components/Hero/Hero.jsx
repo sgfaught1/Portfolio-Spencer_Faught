10c6295c6c3cb099a16c38e4e0972bcf021485af
Spencer Faught <sgfaught1@gmail.com>
2024-05-23 09:45:56 -0700

    Hero Section Added

--- portfolio_Spencer-Faught/src/components/Hero/Hero.jsx
+++ portfolio_Spencer-Faught/src/components/Hero/Hero.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css"
 
-import (getImageUrl)
 
 export default function Hero() {
     return (
-        <section>
-            <div>
-                <h1>Hi, I'm Spencer</h1>
-                <p>I'm a front-end developer with 2 years of experience with frontend languages, libraries, and frameworks. Reach out if you'd like to learn more!</p>
-                <a href="mailto:sgfaught1@gmail.com">Contact Me</a>
+        <section className={styles.container}>
+            <div className = {styles.content}>
+                <h1 className={styles.title}>Hi, I'm Spencer</h1>
+                <p className={styles.description}>I'm a front-end developer with 2 years of experience with frontend languages, libraries, and frameworks. Reach out if you'd like to learn more!</p>
+                <a className={styles.contactBtn} href="mailto:sgfaught1@gmail.com">Contact Me</a>
                 </div>
-                <img src={getImageUrl} alt="" />
+                <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt="hero image of me" />
+                <div className = {styles.topBlur}>
+                </div>
+                <div className = {styles.bottomBlur}></div>
         </section>
     )
 }
