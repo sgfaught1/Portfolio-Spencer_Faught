15097437cb66ec19641e084b45f031c4ec552bbe
Spencer Faught <sgfaught1@gmail.com>
2024-05-24 09:36:59 -0700

    Edit images

--- portfolio_Spencer-Faught/src/components/About/About.jsx
+++ portfolio_Spencer-Faught/src/components/About/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
         <section className = {styles.container} id = 'about'>
             <h2 className={styles.title}>About</h2>
             <div className={styles.content}>
-                <img className={styles.aboutImage} src={getImageUrl("about/aboutImage.png")} alt="Me with a laptop" />
+                <img className={styles.aboutImage} src={getImageUrl("about/aboutImage.svg")} alt="Me with a laptop" />
                 <ul className={styles.aboutItems}>
                     <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
