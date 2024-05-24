3d41fb2ce2e995d76a0d332a01f267992e2205a4
Spencer Faught <sgfaught1@gmail.com>
2024-05-23 17:25:45 -0700

    Added Contact section

--- portfolio_Spencer-Faught/src/components/Contact/Contact.jsx
+++ portfolio_Spencer-Faught/src/components/Contact/Contact.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import styles from "./Contact.module.css"
+import { getImageUrl } from '../../utils'
 
 export default function Contact(){
   return (
-    <div>Contact</div>
+    <footer id = 'contact' className={styles.container}>
+        <div className={styles.text}>
+            <h2>Contact</h2>
+            <p>Feel free to reach out!</p>
+        </div>
+        <ul className={styles.links}>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/emailIcon.png")} alt="Email Icon" />
+                <a href="mailto:sgfaught1@gmail.com">sgfaught1@gmail.com</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn Icon" />
+                <a href="https://www.linkedin.com/in/spencer-faught-4b1960152/?trk=opento_sprofile_details">linkedin.com/spencer-faught</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/githubIcon.png")} alt="Github Icon" />
+                <a href="https://github.com/sgfaught1">github.com/sgfaught1</a>
+            </li>
+        </ul>
+    </footer>
   )
 }
