d0542431101beac9b9d71573c96f4acab344dd9c
Spencer Faught <sgfaught1@gmail.com>
2024-05-23 17:28:33 -0700

    Updated Contact ME button

--- portfolio_Spencer-Faught/src/components/Hero/Hero.jsx
+++ portfolio_Spencer-Faught/src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
             <div className = {styles.content}>
                 <h1 className={styles.title}>Hi, I'm Spencer</h1>
                 <p className={styles.description}>I'm a front-end developer with 2 years of experience with frontend languages, libraries, and frameworks. Reach out if you'd like to learn more!</p>
-                <a className={styles.contactBtn} href="mailto:sgfaught1@gmail.com">Contact Me</a>
+                <a className={styles.contactBtn} href="#contact">Contact Me</a>
                 </div>
                 <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt="hero image of me" />
                 <div className = {styles.topBlur}>
