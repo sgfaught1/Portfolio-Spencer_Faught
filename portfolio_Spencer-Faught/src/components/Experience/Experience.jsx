46dc4cea5e4ca55f1350e381bf1156c0af3e0bdf
Spencer Faught <sgfaught1@gmail.com>
2024-05-24 08:11:55 -0700

    GH pages added to JSON

--- portfolio_Spencer-Faught/src/components/Experience/Experience.jsx
+++ portfolio_Spencer-Faught/src/components/Experience/Experience.jsx
@@ -7,7 +7,7 @@ import history from '../../data/history.json'
 export default function Experience() {
     return (
         <section className={styles.container} id='experience'>
-            <h2 className={styles.title}>Experience</h2>
+            <h2 className={styles.title}>Skills</h2>
             <div className={styles.content}>
                 <div className={styles.skills}>
                     {skills.map((skill, id) => {
